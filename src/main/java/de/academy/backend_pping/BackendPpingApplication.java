030280774f568f19c7f8788ac2465a20480792f0
AndreasAkeret <andreas.akeret@brightstraining.com>
2023-04-20 09:31:33 +0200

    Initial commit

---
+++ src/main/java/de/academy/backend_pping/BackendPpingApplication.java
@@ -0,0 +1,13 @@
+package de.academy.backend_pping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPpingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPpingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/academy/backend_pping/BackendPpingApplicationTests.java
@@ -0,0 +1,13 @@
+package de.academy.backend_pping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPpingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
