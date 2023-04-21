3e0d2a1ddf153968a803c3f87f5b1026137ba98a
BennyLutzBrights <benjamin.lutz936@gmail.com>
2023-04-21 10:04:39 +0200

    foodOptionLayers first definition

---
+++ src/main/java/de/academy/backend_pping/break_group/foodoptions/FoodOption.java
@@ -0,0 +1,79 @@
+package de.academy.backend_pping.break_group.foodoptions;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class FoodOption {
+
+    @Id
+    private Long id;
+
+    private String name;
+    private String address;
+    private String link;
+    private String timeDuration;
+    private int costs;
+    //    private String tags;
+    //    private String additionalInfos;
+
+
+    public FoodOption() {
+    }
+    public FoodOption(Long id, String name, String address, String link, String timeDuration, int costs) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.link = link;
+        this.timeDuration = timeDuration;
+        this.costs = costs;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getTimeDuration() {
+        return timeDuration;
+    }
+
+    public void setTimeDuration(String timeDuration) {
+        this.timeDuration = timeDuration;
+    }
+
+    public int getCosts() {
+        return costs;
+    }
+
+    public void setCosts(int costs) {
+        this.costs = costs;
+    }
+}
---
+++ src/main/java/de/academy/backend_pping/break_group/foodoptions/FoodOptionService.java
@@ -0,0 +1,16 @@
+package de.academy.backend_pping.break_group.foodoptions;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FoodOptionService {
+
+    @Autowired
+    private FoodOptionRepository foodOptionRepository;
+    public List<FoodOption> getFoodOptions() {
+        return (List<FoodOption>) foodOptionRepository.findAll();
+    }
+}
