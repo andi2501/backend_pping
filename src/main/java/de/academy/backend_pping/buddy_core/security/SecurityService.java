ce9ce640ebd400d40b323e934fc8c9d534a24321
CorinnaRi <corinna.riegelein@gmail.com>
2023-04-21 14:28:26 +0200

    Spring Security Tests (Klassen auskommentiert)

---
+++ src/main/java/de/academy/backend_pping/buddy_core/security/SecurityService.java
@@ -0,0 +1,35 @@
+//package de.academy.backend_pping.buddy_core.security;
+//
+//import de.academy.backend_pping.buddy_core.user.UserEntity;
+//import de.academy.backend_pping.buddy_core.user.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.List;
+//
+//@Service
+//public class SecurityService implements UserDetailsService {
+//
+//    private final UserRepository userRepository;
+//
+//    @Autowired
+//    public SecurityService(UserRepository userRepository){
+//        this.userRepository = userRepository;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//
+//        UserEntity userEntity = userRepository.findByUsername(username);
+//        if(userEntity == null) {
+//            throw new UsernameNotFoundException("User " + username + " not found");
+//        }
+//        return new org.springframework.security.core.userdetails.User(userEntity.getUsername(), userEntity.getPassword(), List.of());
+//    }
+//
+//
+//
+//}
---
+++ src/main/java/de/academy/backend_pping/buddy_core/security/WebSecurityConfiguration.java
@@ -0,0 +1,28 @@
+//package de.academy.backend_pping.buddy_core.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/", "/api/registration").permitAll() // neue Zeile: Zugriff aller auf Home und Register
+//                .antMatchers(HttpMethod.POST, "/api/registration").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .loginProcessingUrl("/api/login")
+//                .permitAll()
+//                .and()
+//                .logout().logoutSuccessUrl("/").and() // neue Zeile: nach Logout Weiterleitung zu Home (statt Login)
+//                .httpBasic();
+//    }
+//
+//
+//}
