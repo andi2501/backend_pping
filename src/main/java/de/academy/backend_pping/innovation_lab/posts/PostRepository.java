a526fff8aca35015e08a237ba67b24083e5c84fd
Tim Steinkraus <tim@steinkraus.eu>
2023-04-21 14:33:55 +0200

    Post Repository

---
+++ src/main/java/de/academy/backend_pping/innovation_lab/posts/PostRepository.java
@@ -0,0 +1,14 @@
+package de.academy.backend_pping.innovation_lab.posts;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends CrudRepository<Post, Long> {
+    List<Post> findAll();
+    List<Post> findPostsByAuthorIdOrderByCreationTimestampDesc(long id);
+    Optional<Post> findPostById(long id);
+}
