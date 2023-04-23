4e57b51c6491592ae76db796b12abb3c2e41cd37
CorinnaRi <corinna.riegelein@gmail.com>
2023-04-23 15:05:40 +0200

    check if username taken and return response entity to frontend

--- src/main/java/de/academy/backend_pping/buddy_core/user/RegistrationController.java
+++ src/main/java/de/academy/backend_pping/buddy_core/user/RegistrationController.java
@@ -1,6 +1,9 @@
 package de.academy.backend_pping.buddy_core.user;
 
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
@@ -17,13 +20,19 @@ public class RegistrationController {
     }
 
     @PostMapping
-    public @ResponseBody UserDTO registerUser(@RequestBody UserDTO userDtoReceived){
+    public @ResponseBody ResponseEntity<?> registerUser(@RequestBody UserDTO userDtoReceived){
+
+        if (userService.existsByUsername(userDtoReceived.getUsername())) {
+            // Return an error response to the frontend
+            return new ResponseEntity<>("Username already taken", HttpStatus.BAD_REQUEST);
+        }
+
 
        UserEntity newUser = userService.registerUser(userDtoReceived.getUsername(), userDtoReceived.getPassword());
 
        UserDTO userDtoRegistered = new UserDTO(newUser.getUsername(), newUser.getPassword(), newUser.getId());
        userDtoRegistered.setPassword(null);
 
-       return userDtoRegistered;
+       return new ResponseEntity<>(userDtoRegistered, HttpStatus.OK);
     }
 }
