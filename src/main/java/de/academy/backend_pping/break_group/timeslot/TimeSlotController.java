5d2122cf50414b9a07305f956b1233debdec6cd3
Silvia Heckert <silvia.heckert@brightstraining.com>
2023-04-26 14:09:56 +0200

    Cookie Session token added to PutMapping in TimeSlotController.

--- src/main/java/de/academy/backend_pping/break_group/timeslot/TimeSlotController.java
+++ src/main/java/de/academy/backend_pping/break_group/timeslot/TimeSlotController.java
@@ -2,7 +2,9 @@ package de.academy.backend_pping.break_group.timeslot;
 
 import de.academy.backend_pping.break_group.foodoptions.FoodOptionDTO;
 import de.academy.backend_pping.break_group.foodoptions.FoodOptionService;
+import de.academy.backend_pping.buddy_core.session.SessionService;
 import de.academy.backend_pping.buddy_core.user.UserEntity;
+import de.academy.backend_pping.buddy_core.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +23,12 @@ public class TimeSlotController {
     @Autowired
     private FoodOptionService foodOptionService;
 
+    @Autowired
+    private SessionService sessionService;
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public List<TimeSlotDTO> getAllTimeSlots() {
         return timeSlotService.getAllTimeSlots();
@@ -34,23 +42,30 @@ public class TimeSlotController {
     @PostMapping
     public @ResponseBody TimeSlotDTO addTimeSlotToFoodOption
             (@CookieValue(value = "session", defaultValue = "1") String token,
-    @RequestBody FoodOptionDTO foodOptionDTO, @RequestBody TimeSlotDTO timeSlotDTO) {
-        Random random = new Random();
-        int randomNumber = random.nextInt(10000 + 1);
-        String randNumber = Integer.toString(randomNumber);
-        UserEntity currentUser = new UserEntity("user2323" + randNumber, "password23");
-        // TODO sessionService
+             @RequestBody FoodOptionDTO foodOptionDTO, @RequestBody TimeSlotDTO timeSlotDTO) {
+        long userID = sessionService.getUserId(token);
+        UserEntity currentUser = userService.findById(userID);
         return timeSlotService.addTimeSlotToFoodOption(timeSlotDTO, foodOptionService
                 .getFoodOptionByName(foodOptionDTO.getName()), currentUser);
     }
+//    public @ResponseBody TimeSlotDTO addTimeSlotToFoodOption
+//            (@CookieValue(value = "session", defaultValue = "1") String token,
+//    @RequestBody FoodOptionDTO foodOptionDTO, @RequestBody TimeSlotDTO timeSlotDTO) {
+////        Random random = new Random();
+////        int randomNumber = random.nextInt(10000 + 1);
+////        String randNumber = Integer.toString(randomNumber);
+//        UserEntity currentUser = new SessionService ("user2323" + randNumber, "password23");
+//        // TODO sessionService
+//        return timeSlotService.addTimeSlotToFoodOption(timeSlotDTO, foodOptionService
+//                .getFoodOptionByName(foodOptionDTO.getName()), currentUser);
+//    }
 
     @PutMapping
-    public @ResponseBody TimeSlotDTO updateExistingTimeSlot(@RequestBody TimeSlotDTO oldTimeSlotDTO) {
-        Random random = new Random();
-        int randomNumber = random.nextInt(10000 + 1);
-        String randNumber = Integer.toString(randomNumber);
-        UserEntity currentUser = new UserEntity("user2323" + randNumber, "password23");
-        TimeSlotDTO newTimeSlotDto = timeSlotService.update(currentUser, oldTimeSlotDTO);
-        return newTimeSlotDto;
+    public @ResponseBody TimeSlotDTO updateExistingTimeSlot
+            (@CookieValue(value = "session", defaultValue = "1") String token,
+             @RequestBody TimeSlotDTO oldTimeSlotDTO) {
+            long userID = sessionService.getUserId(token);
+            UserEntity currentUser = userService.findById(userID);
+        return timeSlotService.update(currentUser, oldTimeSlotDTO);
     }
 }
